afc595e5b62de8b0e846b29650f1d8f3db87b894
wesleyhuang23 <wesleyhuang23@gmail.com>
2016-11-02 17:20:41 -0600

    recomendations page up

--- js/app.js
+++ js/app.js
@@ -4,18 +4,23 @@ angular.module('oracApp', ['ui.router'])
   $urlRouterProvider.otherwise('/');
 
   $stateProvider.state('home', {
+    
     templateUrl: './templates/home.html',
     url :'/'
   }).state('oxidation',{
+
     templateUrl:'./templates/oxidation.html',
     url: '/oxidation'
   }).state('anti-oxidants',{
+
     templateUrl: './templates/anti-oxidants.html',
     url:'/anti-oxidants',
     controller: 'oracCtrl'
+
   }).state('recomendations', {
     templateUrl: './templates/recomendations.html',
     url: '/recomendations'
+
   }).state('contact', {
     templateUrl:'./templates/contact.html',
     url: '/contact'
